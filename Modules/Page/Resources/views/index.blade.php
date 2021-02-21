7a1fd4036a6d684e6f6e8588b23c7e6310baa593
Islam Gad <islaam.gaad@gmail.com>
2021-02-21 18:24:06 +0200

    adding Pixel code

--- Modules/Page/Resources/views/index.blade.php
+++ Modules/Page/Resources/views/index.blade.php
@@ -16,6 +16,71 @@
         <!-- meta -->
         <meta name="description" content="description">
         <meta name="keywords" content="keywords">
+
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '900734270680681');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id=900734270680681&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Facebook Pixel Code -->
+
+
+
+        <!-- Snap Pixel Code -->
+        <script type='text/javascript'>
+            (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+            {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+                a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+                r.src=n;var u=t.getElementsByTagName(s)[0];
+                u.parentNode.insertBefore(r,u);})(window,document,
+                'https://sc-static.net/scevent.min.js');
+
+            snaptr('init', '6650f7cb-4eb8-4136-b9e0-d08672e8bf55', {
+                'user_email': '__INSERT_USER_EMAIL__'
+            });
+
+            snaptr('track', 'PAGE_VIEW');
+
+        </script>
+        <!-- End Snap Pixel Code -->
+
+
+
+        <!-- Global site tag (gtag.js) - Google Ads: 604548133 -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-604548133"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-604548133');
+        </script>
+
+
+        <!-- Twitter universal website tag code -->
+        <script>
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            // Insert Twitter Pixel ID and Standard Event data below
+            twq('init','o5joa');
+            twq('track','PageView');
+        </script>
+        <!-- End Twitter universal website tag code -->
+
+
+
     </head>
 
     <body>
@@ -580,8 +645,13 @@
                         url: form.attr('action'),
                         data: form.serialize(),
                         success: function (data) {
+                                
                             $('#form-container').html($('#thankyou').html());
+                            gtag('event', 'conversion', {
+                                'send_to': 'AW-604548133/54R_CPDT_PYBEKXYoqAC'
+                            });
                         },
+
                         error: function (data) {
                             $('#form-container').html($('#thankyou').html());
                         },
