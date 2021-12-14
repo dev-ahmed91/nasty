7192289abf0ae5ab72d7b88ad1c7b7e74eb7bdcc
Islam Gad <islaam.gaad@gmail.com>
2021-12-14 15:15:37 +0200

    fixing page controller

--- Modules/Page/routes.php
+++ Modules/Page/routes.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 $namespace = 'Modules\\Page\\Controllers\\';
 
 Route::prefix('/')->attribute('namespace', $namespace.'Web')->group( function () {
-    Route::get('/', 'Pagecontroller@index')->name('web.home.index');
+    Route::get('/', 'PageController@index')->name('web.home.index');
 });
 
